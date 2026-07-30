c57a08e32a2ea5c244250dac133944b685f58634
Johnny LaPore <johnlapore4@gmail.com>
2026-07-29 20:24:03 -0500

    added basic CMake funcionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "a.h"
 
 int main()
 {
-    std::cout >> "This is a C++ project!" std::endl;
+    std::cout << "This is a C++ project!" << std::endl;
+    a();
+    std::cin.get();
     return 0;
 }
